48ff271656cbfa3d9ccd04faf15cc5add8267056
Shada <43104909+shada12@users.noreply.github.com>
2022-01-12 19:06:35 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,8 +2,8 @@
 
 <br><h1 class="text-center">Welcome to Algonquin College Social Media Website</h1>
   <hr><br>  
-   <p> &#160; &#160; &#160; &#160;If you have never used this before, you have to <a href="NewUser.php" style='color:blue'> sign up </a> first. </p>
-   <p> &#160; &#160; &#160; &#160;If you have already signed up you can <a href="Login.php" style='color:blue'>log in </a> now. </p>
+   <p>If you have never used this before, you have to <a href="NewUser.php" style='color:blue'> sign up </a> first. </p>
+   <p>If you have already signed up you can <a href="Login.php" style='color:blue'>log in </a> now. </p>
 
 
 <?php include "ProjectCommon/Footer.php" ?>
